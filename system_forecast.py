36d56d51c38caa7eb7c69ac1d25400d01ac55e2e
ataozarslan@hotmail.com <ataozarslan@hotmail.com>
2026-07-06 13:50:32 +0000

    Update system forecast dependencies

--- system_forecast.py
+++ system_forecast.py
@@ -3,8 +3,6 @@ import time
 import requests
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 import psycopg2
 import pytz
 from sqlalchemy import create_engine
